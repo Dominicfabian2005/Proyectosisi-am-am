101eb6322b943a7a89413dbe6527e06996ffe0e3
20242260 <20242260@Belleua05>
2025-08-10 20:48:50 -0400

    Primer commit del proyecto Heladería Blizz

---
+++ src/proyectofinalhelado/HeladoCajita.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinalhelado;
+
+/**
+ *
+ * @author 20242260
+ */
+
+
+public class HeladoCajita extends Producto {
+    private String tamanio; // Atributo específico
+
+    public HeladoCajita(int id, String nombre, double precio, int proveedor, String tamanio) {
+        super(id, nombre, precio, proveedor);
+        this.tamanio = tamanio;
+    }
+
+    @Override
+    public double calcularPrecioFinal() {
+        double precioAdicional = 0.0;
+        if (this.tamanio.equals("Grande")) {
+            precioAdicional = 1.25;
+        }
+        return getPrecio() + precioAdicional;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+}
